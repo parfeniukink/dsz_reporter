ffcf9dfae990be6631515b567e5974a5a2ca9886
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-01-21 15:30:25 +0200

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +121,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())
